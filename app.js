4900c4396a828edf2a8bdbc62863c68258836a52
h3xar0n <aron@eideldev.com>
2017-07-17 14:51:14 -0700

    Fetch and handle weather data

--- app.js
+++ app.js
@@ -10,15 +10,20 @@ function searchWeather() {
   }
   var http = new XMLHttpRequest();
   // API key hidden in api.js, see README
-  var url = 'api.openweathermap.org/data/2.5/weather?q=' + cityName;
+  var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=imperial&appid=' + apiKey;
   var method = 'GET';
 
   http.open(method, url);
   http.onreadystatechange = function() {
     if (http.readyState == XMLHttpRequest.DONE && http.status === 200) {
       var data = JSON.parse(http.responseText);
+      var weatherData = new Weather(cityName, data.weather[0].description.toUpperCase());
+      weatherData.temperature = data.main.temp;
+      // Temporarily log until showing on UI
+      console.log(weatherData);
     } else if (http.readyState == XMLHttpRequest.DONE) {
       alert('Something went wrong!');
     }
   };
+  http.send();
 }
